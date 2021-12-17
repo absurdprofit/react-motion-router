99e2640df34cae3b7388e87e6bdc8a768fca4c06
Nathan Johnson <nahtanjohn1l@gmail.com>
2021-12-17 00:26:32 -0500

    Added stack to history

--- src/Navigation/common/utils.ts
+++ src/Navigation/common/utils.ts
@@ -1,10 +1,23 @@
 export class History {
+    private _stack: string[] = [];
+    private _next: string | null = null;
+    private _previous: string | null = null;
+    get next() {
+        return this._next;
+    }
+    get previous() {
+        return this._previous;
+    }
     push(route: string) {
+        this._next = route;
         window.history.pushState({}, "", route);
+        this._stack.push(route);
     }
 
     back() {
+        this._previous = this._stack.pop() || null;
         window.history.back();
+        return this._previous;
     }
 }
 export class Navigation {
