d0c3d99a2a5a8701f95b826681b5cbab250ba593
Nathan Johnson <nahtanjohn1l@gmail.com>
2022-07-17 14:53:08 -0500

    Fixed nested router event dispatching bug which caused higher level routers to receive navigation events first

--- example/src/Screens/Nested/index.tsx
+++ example/src/Screens/Nested/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Anchor, Router, Stack } from 'react-motion-router';
+import { Anchor, Navigation, Router, Stack } from 'react-motion-router';
 import Tab from './Tab';
 import '../../css/Nested.css';
 
-const Tab1 = () => <Tab title="Tab 1" />;
-const Tab2 = () => <Tab title="Tab 2"><Anchor href='/' goBack /></Tab>;
+const Tab1 = () => <Tab title="Tab 1"></Tab>;
+const Tab2 = () => <Tab title="Tab 2"></Tab>;
 
-export default class Nested extends React.Component {
+interface NestedState {
+    navigation?: Navigation;
+}
+
+export default class Nested extends React.Component<any, NestedState> {
+    state: NestedState = {}
+
+    pushTab = (tabRoute: string) => {
+        if (!this.state.navigation) return;
+
+    }
     render() {
+        const {navigation} = this.state;
+
         return (
             <div className="tabs-nav">
-                <Anchor href='/'>Tab 1</Anchor>
-                <Anchor href='/tab-2'>Tab 2</Anchor>
-                <Router config={{defaultRoute: '/tabs-2', animation: {type: 'none', duration: 0}}}>
+                <div className="header-layout">
+                    <button onClick={() => navigation?.navigate('/', {}, true)}>Tab 1</button>
+                    <button onClick={() => navigation?.navigate('/tab-2', {}, true)}>Tab 2</button>
+                </div>
+                <Router onMount={(navigation: Navigation) => {
+                    this.setState({navigation});
+                }} config={{animation: {type: 'none', duration: 0}}}>
                     <Stack.Screen component={Tab1} name='Tab 1' path='/' />
                     <Stack.Screen component={Tab2} name='Tab 2' path='/tab-2' />
                 </Router>
