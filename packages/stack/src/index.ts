8b32e9858ef502de8e8d0f57346cc611e7a94473
Nathan Johnson <50880261+nxtexe@users.noreply.github.com>
2024-04-26 22:00:45 -0500

    Delete packages directory

--- packages/stack/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Stack } from './Stack';
-import Router, { RouterProps, RouterState } from './Router';
-import History from './History';
-import Navigation from './Navigation';
-
-export {Stack, Router, Navigation, History};
-export type {RouterState, RouterProps};
