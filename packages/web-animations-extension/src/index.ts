d53f3fe7b1bc9c3594d118ea7335f18fdea76cdf
Nathan Johnson <nahtanjohn1l@gmail.com>
2024-05-09 00:52:17 -0500

    Removed test animation from web animations extension

--- packages/web-animations-extension/src/index.ts
+++ packages/web-animations-extension/src/index.ts
@@ -1,10 +1,4 @@
-import { Animation } from './animation';
-import { GestureTimeline } from './gesture-timeline';
-
 export * from './group-effect';
 export * from './animation';
 export * from './parallel-effect';
 export * from './common/utils';
-const timeline = new GestureTimeline();
-const effect = new KeyframeEffect(document.body, [{transform: 'translateX(0)'}, {transform: 'translateX(100px)'}], {duration: 100});
-new Animation(effect, timeline);
