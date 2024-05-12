94cc7d1d1ce7feab9f0a60d57463005d216f8c6d
Nathan Johnson <nahtanjohn1l@gmail.com>
2024-05-12 10:17:47 -0500

    Updates

---
+++ assets/extendSxProp-v5WqL1EG.js
@@ -0,0 +1,2 @@
+/* LICENSES */
+import{f as u,d as i,a0 as c,a1 as f}from"./index-EZw606xg.js";const x=["sx"],P=t=>{var s,n;const o={systemProps:{},otherProps:{}},r=(s=t==null||(n=t.theme)==null?void 0:n.unstable_sxConfig)!=null?s:c;return Object.keys(t).forEach(e=>{r[e]?o.systemProps[e]=t[e]:o.otherProps[e]=t[e]}),o};function d(t){const{sx:s}=t,n=u(t,x),{systemProps:o,otherProps:r}=P(n);let e;return Array.isArray(s)?e=[o,...s]:typeof s=="function"?e=(...a)=>{const l=s(...a);return f(l)?i({},o,l):o}:e=i({},o,s),i({},r,{sx:e})}export{d as e};
