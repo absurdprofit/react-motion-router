e66faf64d66269d15008d3d219e103c137faa75e
absurdprofit <nahtanjohn1l@gmail.com>
2024-08-18 19:00:12 -0500

    Added comment about commit

--- packages/core/src/common/types.ts
+++ packages/core/src/common/types.ts
@@ -125,7 +125,7 @@ export interface LoadNavigationTransition extends Omit<NavigationTransition, "na
 
 declare global {
     interface NavigateEvent extends Event {
-        commit?(): void;
+        commit?(): void; // not in spec yet, see https://github.com/WICG/navigation-api/issues/66
     }
 }
 
