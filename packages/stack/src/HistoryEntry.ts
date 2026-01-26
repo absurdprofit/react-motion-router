e4044a068c56cf8d3b5389de2be6005d0e772c04
Nathan Johnson <absurdprofit@icloud.com>
2026-01-26 10:13:00 -0500

    Nit: lint fixes

--- packages/stack/src/HistoryEntry.ts
+++ packages/stack/src/HistoryEntry.ts
@@ -1,62 +1,98 @@
 export class HistoryEntry implements Omit<NavigationHistoryEntry, 'url'> {
-  readonly routerId: string;
-  #nativeEntry: NavigationHistoryEntry;
-  readonly index: number;
+  public readonly routerId: string;
+  readonly #nativeEntry: NavigationHistoryEntry;
+  public readonly index: number;
 
-  constructor(nativeEntry: NavigationHistoryEntry, routerId: string, index: number) {
+  constructor(
+    nativeEntry: NavigationHistoryEntry,
+    routerId: string,
+    index: number
+  ) {
     this.#nativeEntry = nativeEntry;
     this.routerId = routerId;
     this.index = index;
   }
 
-  set ondispose(handler: ((this: NavigationHistoryEntry, ev: Event) => any) | null) {
+  public set ondispose(
+    handler: ((this: NavigationHistoryEntry, ev: Event) => void) | null
+  ) {
     this.#nativeEntry.ondispose = handler;
   }
 
-  get ondispose() {
+  public get ondispose() {
     return this.#nativeEntry.ondispose;
   }
 
-  get id() {
+  public get id() {
     return this.#nativeEntry.id;
   }
 
-  get globalIndex() {
+  public get globalIndex() {
     return this.#nativeEntry.index;
   }
 
-  get url() {
+  public get url() {
     if (!this.#nativeEntry.url) return null;
     return new URL(this.#nativeEntry.url);
   }
 
-  get key() {
+  public get key() {
     return this.#nativeEntry.key;
   }
 
-  get sameDocument() {
+  public get sameDocument() {
     return this.#nativeEntry.sameDocument;
   }
 
-  addEventListener<K extends keyof NavigationHistoryEntryEventMap>(type: K, listener: (this: NavigationHistoryEntry, ev: NavigationHistoryEntryEventMap[K]) => any, options?: boolean | AddEventListenerOptions): () => void;
-  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): () => void;
-  addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): () => void {
+  public addEventListener<K extends keyof NavigationHistoryEntryEventMap>(
+    type: K,
+    listener: (
+      this: NavigationHistoryEntry,
+      ev: NavigationHistoryEntryEventMap[K]
+    ) => void,
+    options?: boolean | AddEventListenerOptions
+  ): () => void;
+  public addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions
+  ): () => void;
+  public addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions
+  ): () => void {
     this.#nativeEntry.addEventListener(type, listener, options);
 
     return () => this.#nativeEntry.removeEventListener(type, listener, options);
   }
 
-  removeEventListener<K extends keyof NavigationHistoryEntryEventMap>(type: K, listener: (this: NavigationHistoryEntry, ev: NavigationHistoryEntryEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
-  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
-  removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void {
+  public removeEventListener<K extends keyof NavigationHistoryEntryEventMap>(
+    type: K,
+    listener: (
+      this: NavigationHistoryEntry,
+      ev: NavigationHistoryEntryEventMap[K]
+    ) => void,
+    options?: boolean | EventListenerOptions
+  ): void;
+  public removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions
+  ): void;
+  public removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions
+  ): void {
     this.#nativeEntry.addEventListener(type, listener, options);
   }
 
-  dispatchEvent(event: Event): boolean {
+  public dispatchEvent(event: Event): boolean {
     return this.#nativeEntry.dispatchEvent(event);
   }
 
-  getState<T>() {
+  public getState<T>() {
     return this.#nativeEntry.getState() as T;
   }
 }
