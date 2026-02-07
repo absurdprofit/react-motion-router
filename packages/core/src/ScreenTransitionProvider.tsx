cb0bf0d9d1518b41debeaa5ba0f46dc4d89f31c5
Nathan Johnson <absurdprofit@icloud.com>
2026-02-07 13:05:32 -0500

    Refactor ScreenTransitionProvider as function component

--- packages/core/src/ScreenTransitionProvider.tsx
+++ packages/core/src/ScreenTransitionProvider.tsx
@@ -1,51 +1,56 @@
 import { ScreenTransitionLayerContext } from './ScreenTransitionLayerContext';
 import { AnimationEffectFactory } from './common/types';
-import { Component, RefObject } from 'react';
+import { RefObject, useContext, useImperativeHandle } from 'react';
 import { FIRST_INDEX } from './common/constants';
 
-interface ScreenTransitionProviderProps {
+export interface ScreenTransitionProviderHandle {
+  index: number;
+  exiting: boolean;
+  readonly animationEffect: AnimationEffect | null;
+  readonly screenElementRef: RefObject<HTMLElement | null>;
+}
+
+export interface ScreenTransitionProviderProps {
   viewTransitionName: string;
   animation?: AnimationEffectFactory;
   children: React.ReactNode
   screenElementRef: RefObject<HTMLElement | null>;
+  ref?: RefObject<ScreenTransitionProviderHandle | null>;
 }
 
-export class ScreenTransitionProvider extends Component<
-  ScreenTransitionProviderProps
-> {
-  public static readonly contextType = ScreenTransitionLayerContext;
-  public declare context: React.ContextType<
-    typeof ScreenTransitionLayerContext
-  >;
-  public index = FIRST_INDEX;
-  public exiting = false;
+export function ScreenTransitionProvider(props: ScreenTransitionProviderProps) {
+  const context = useContext(ScreenTransitionLayerContext);
 
-  public get screenElementRef() {
-    return this.props.screenElementRef;
-  }
+  useImperativeHandle(
+    props.ref,
+    () => ({
+      index: FIRST_INDEX,
+      exiting: false,
+      get screenElementRef() {
+        return props.screenElementRef;
+      },
+      get animationEffect() {
+        const animationEffectFactory = props.animation;
+        const { animation, direction, hasUAVisualTransition } = context;
+        const { timeline, playbackRate } = animation;
+        const { index, exiting, screenElementRef } = this;
+        const { viewTransitionName } = props;
+        const screens = context.screens.map(screen => screen.current?.name);
 
-  public get animationEffect() {
-    const animationEffectFactory = this.props.animation;
-    const { animation, direction, hasUAVisualTransition } = this.context;
-    const { timeline, playbackRate } = animation;
-    const { index, exiting, screenElementRef } = this;
-    const { viewTransitionName } = this.props;
-    const screens = this.context.screens.map(screen => screen.current?.name);
+        return animationEffectFactory?.({
+          ref: screenElementRef.current,
+          index,
+          screens,
+          exiting,
+          timeline,
+          direction,
+          playbackRate,
+          viewTransitionName,
+          hasUAVisualTransition,
+        }) ?? null;
+      },
+    })
+  );
 
-    return animationEffectFactory?.({
-      ref: screenElementRef.current,
-      index,
-      screens,
-      exiting,
-      timeline,
-      direction,
-      playbackRate,
-      viewTransitionName,
-      hasUAVisualTransition,
-    }) ?? null;
-  }
-
-  public render() {
-    return this.props.children;
-  }
+  return props.children;
 }
